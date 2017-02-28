56e011138a7845100a897805ffec24e197a87531
Teodor Sigaev <teodor@sigaev.ru>
2017-02-28 13:45:43 +0300

    correct work of unnary plus/minus

--- src/backend/utils/adt/jsonpath.c
+++ src/backend/utils/adt/jsonpath.c
@@ -95,7 +95,6 @@ flattenJsonPathParseItem(StringInfo buf, JsonPathParseItem *item,
 		case jpiFilter:
 		case jpiIsUnknown:
 		case jpiNot:
-		case jpiPlus:
 		case jpiMinus:
 		case jpiExists:
 			{
@@ -482,7 +481,6 @@ jspInitByBuffer(JsonPathItem *v, char *base, int32 pos)
 		case jpiExists:
 		case jpiIsUnknown:
 		case jpiMinus:
-		case jpiPlus:
 		case jpiFilter:
 			read_int32(v->arg, base, pos);
 			break;
@@ -507,7 +505,6 @@ jspGetArg(JsonPathItem *v, JsonPathItem *a)
 		v->type == jpiNot ||
 		v->type == jpiIsUnknown ||
 		v->type == jpiExists ||
-		v->type == jpiPlus ||
 		v->type == jpiMinus
 	);
 
