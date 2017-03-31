d98ab5b1d8b379a489fe9a82b8ba00526c3184e5
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-03-31 13:19:48 +0300

    Add jsonpath JsonbWrapInBinary()

--- src/backend/utils/adt/jsonpath_exec.c
+++ src/backend/utils/adt/jsonpath_exec.c
@@ -159,6 +159,17 @@ JsonbInitBinary(JsonbValue *jbv, Jsonb *jb)
 	return jbv;
 }
 
+static inline JsonbValue *
+JsonbWrapInBinary(JsonbValue *jbv, JsonbValue *out)
+{
+	Jsonb	   *jb = JsonbValueToJsonb(jbv);
+
+	if (!out)
+		out = palloc(sizeof(*out));
+
+	return JsonbInitBinary(out, jb);
+}
+
 /********************Execute functions for JsonPath***************************/
 
 /*
@@ -1963,7 +1974,6 @@ static inline JsonbValue *
 wrapItem(JsonbValue *jbv)
 {
 	JsonbParseState *ps = NULL;
-	Jsonb	   *jb;
 	JsonbValue	jbvbuf;
 	int			type = JsonbType(jbv);
 
@@ -1977,14 +1987,7 @@ wrapItem(JsonbValue *jbv)
 	pushJsonbValue(&ps, WJB_ELEM, jbv);
 	jbv = pushJsonbValue(&ps, WJB_END_ARRAY, NULL);
 
-	jb = JsonbValueToJsonb(jbv);
-
-	jbv = palloc(sizeof(*jbv));
-	jbv->type = jbvBinary;
-	jbv->val.binary.data = &jb->root;
-	jbv->val.binary.len = VARSIZE(jb) - VARHDRSZ;
-
-	return jbv;
+	return JsonbWrapInBinary(jbv, NULL);
 }
 
 static inline JsonPathExecResult
