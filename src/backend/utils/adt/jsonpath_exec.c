c881be420708733a0f042a701e059a01b9d15bcf
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-03-31 14:02:18 +0300

    Mark some jsonpath functions as inline

--- src/backend/utils/adt/jsonpath_exec.c
+++ src/backend/utils/adt/jsonpath_exec.c
@@ -38,7 +38,7 @@ typedef struct JsonValueListIterator
 
 #define JsonValueListIteratorEnd ((ListCell *) -1)
 
-static JsonPathExecResult recursiveExecute(JsonPathExecContext *cxt,
+static inline JsonPathExecResult recursiveExecute(JsonPathExecContext *cxt,
 										   JsonPathItem *jsp, JsonbValue *jb,
 										   JsonValueList *found);
 
@@ -324,7 +324,7 @@ computeJsonPathItem(JsonPathExecContext *cxt, JsonPathItem *item, JsonbValue *va
  * as alias to jbvBinary
  */
 #define jbvScalar jbvBinary
-static int
+static inline int
 JsonbType(JsonbValue *jb)
 {
 	int type = jb->type;
@@ -544,7 +544,7 @@ compareDatetime(Datum val1, Oid typid1, Datum val2, Oid typid2, bool *error)
 	return DatumGetInt32(DirectFunctionCall2(cmpfunc, val1, val2));
 }
 
-static JsonPathExecResult
+static inline JsonPathExecResult
 checkEquality(JsonbValue *jb1, JsonbValue *jb2, bool not)
 {
 	bool	eq = false;
@@ -1977,7 +1977,7 @@ recursiveExecuteUnwrap(JsonPathExecContext *cxt, JsonPathItem *jsp,
  * Wrap a non-array SQL/JSON item into an array for applying array subscription
  * path steps in lax mode.
  */
-static JsonbValue *
+static inline JsonbValue *
 wrapItem(JsonbValue *jbv)
 {
 	JsonbParseState *ps = NULL;
@@ -2015,12 +2015,10 @@ wrapItem(JsonbValue *jbv)
 	return JsonbWrapInBinary(jbv, NULL);
 }
 
-static JsonPathExecResult
+static inline JsonPathExecResult
 recursiveExecute(JsonPathExecContext *cxt, JsonPathItem *jsp, JsonbValue *jb,
 				 JsonValueList *found)
 {
-	check_stack_depth();
-
 	if (cxt->lax)
 	{
 		switch (jsp->type)
