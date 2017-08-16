b23bb88c7bedbc6d86e3b598d60af83a0d1e0675
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-08-16 16:03:42 +0300

    Add json support for JSON_EXISTS, JSON_VALUE, JSON_QUERY

--- src/backend/parser/parse_expr.c
+++ src/backend/parser/parse_expr.c
@@ -4556,15 +4556,10 @@ static Node *
 transformJsonFuncExpr(ParseState *pstate, JsonFuncExpr *func)
 {
 	JsonExpr   *jsexpr = transformJsonExprCommon(pstate, func);
-	Node	   *expr = jsexpr->formatted_expr ?
-					   jsexpr->formatted_expr : jsexpr->raw_expr;
-	const char *func_name = NULL;
 
 	switch (func->op)
 	{
 		case IS_JSON_VALUE:
-			func_name = "JSON_VALUE";
-
 			transformJsonFuncExprOutput(pstate, func, jsexpr);
 
 			jsexpr->returning.format.type = JS_FORMAT_DEFAULT;
@@ -4580,8 +4575,6 @@ transformJsonFuncExpr(ParseState *pstate, JsonFuncExpr *func)
 			break;
 
 		case IS_JSON_QUERY:
-			func_name = "JSON_QUERY";
-
 			transformJsonFuncExprOutput(pstate, func, jsexpr);
 
 			jsexpr->wrapper = func->wrapper;
@@ -4590,8 +4583,6 @@ transformJsonFuncExpr(ParseState *pstate, JsonFuncExpr *func)
 			break;
 
 		case IS_JSON_EXISTS:
-			func_name = "JSON_EXISTS";
-
 			jsexpr->returning.format.type = JS_FORMAT_DEFAULT;
 			jsexpr->returning.format.encoding = JS_ENC_DEFAULT;
 			jsexpr->returning.format.location = -1;
@@ -4600,12 +4591,6 @@ transformJsonFuncExpr(ParseState *pstate, JsonFuncExpr *func)
 
 			break;
 	}
-
-	if (exprType(expr) != JSONBOID)
-		ereport(ERROR,
-				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-				 errmsg("%s() is not yet implemented for json type", func_name),
-				 parser_errposition(pstate, func->location)));
 
 	return (Node *) jsexpr;
 }
